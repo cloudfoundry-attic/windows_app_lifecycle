99fe34321358841c95b5929fc2cfe8b7a802ef62
Matthew Horan <mhoran@pivotal.io>
2016-12-12 12:17:37 -0500

    Remove diego-release submodule
    We now add the diego-sshd in via the bosh release.
    
    [#135953721]
    
    Signed-off-by: Amin Jamali <ajamali@pivotal.io>

--- scripts/make.bat
+++ scripts/make.bat
@@ -17,19 +17,12 @@ bin\nuget restore || exit /b 1
 SET GOPATH=%CD%\diego-release
 SET PATH=%GOPATH%\bin;%PATH%
 
-pushd %GOPATH%\src\code.cloudfoundry.org\diego-ssh
-      go get github.com/Sirupsen/logrus
-      go install github.com/onsi/ginkgo/ginkgo
-      ginkgo -r -noColor -skipPackage="ssh-proxy" . || exit /b 1
-popd
-go build -o diego-sshd.exe code.cloudfoundry.org/diego-ssh/cmd/sshd || exit /b 1
-
 MSBuild WindowsAppLifecycle.sln /t:Rebuild /p:Configuration=Release || exit /b 1
 packages\nspec.0.9.68\tools\NSpecRunner.exe Healthcheck.Tests\bin\Release\Healthcheck.Tests.dll || exit /b 1
 packages\nspec.0.9.68\tools\NSpecRunner.exe Builder.Tests\bin\Release\BuilderTests.dll || exit /b 1
 packages\nspec.0.9.68\tools\NSpecRunner.exe Launcher.Tests\bin\Release\LauncherTests.dll || exit /b 1
 packages\nspec.0.9.68\tools\NSpecRunner.exe WebAppServer.Tests\bin\Release\WebAppServer.Tests.dll || exit /b 1
-bin\bsdtar -czvf windows_app_lifecycle.tgz --exclude log -C Builder\bin . -C ..\..\Launcher\bin . -C ..\..\Healthcheck\bin . -C ..\..\WebAppServer\bin . -C ..\.. diego-sshd.exe || exit /b 1
+bin\bsdtar -czvf windows_app_lifecycle.tgz --exclude log -C Builder\bin . -C ..\..\Launcher\bin . -C ..\..\Healthcheck\bin . -C ..\..\WebAppServer\bin . || exit /b 1
 for /f "tokens=*" %%a in ('git rev-parse --short HEAD') do (
     set VAR=%%a
     )
