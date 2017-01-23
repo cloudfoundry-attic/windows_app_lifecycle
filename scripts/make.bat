e716dbfc6d95ec7a56ef8f32a6bccfecafa06551
Sunjay Bhatia <sbhatia@pivotal.io>
2017-01-23 18:04:31 -0500

    cleanup make.bat

--- scripts/make.bat
+++ scripts/make.bat
@@ -3,7 +3,6 @@ SET PATH=%PATH%;%WINDIR%\Microsoft.NET\Framework64\v4.0.30319;%WINDIR%\SysNative
 where msbuild
 if errorLevel 1 ( echo "msbuild was not found on PATH" && exit /b 1 )
 
-
 :: enable some features
 dism /online /Enable-Feature /FeatureName:IIS-WebServer /All /NoRestart
 dism /online /Enable-Feature /FeatureName:IIS-WebSockets /All /NoRestart
@@ -14,9 +13,6 @@ rmdir /S /Q output
 rmdir /S /Q packages
 bin\nuget restore || exit /b 1
 
-SET GOPATH=%CD%\diego-release
-SET PATH=%GOPATH%\bin;%PATH%
-
 MSBuild WindowsAppLifecycle.sln /t:Rebuild /p:Configuration=Release || exit /b 1
 packages\nspec.0.9.68\tools\NSpecRunner.exe Builder.Tests\bin\Release\BuilderTests.dll || exit /b 1
 packages\nspec.0.9.68\tools\NSpecRunner.exe Launcher.Tests\bin\Release\LauncherTests.dll || exit /b 1
@@ -24,7 +20,7 @@ packages\nspec.0.9.68\tools\NSpecRunner.exe WebAppServer.Tests\bin\Release\WebAp
 bin\bsdtar -czvf windows_app_lifecycle.tgz --exclude log -C Builder\bin . -C ..\..\Launcher\bin . -C ..\..\WebAppServer\bin . || exit /b 1
 for /f "tokens=*" %%a in ('git rev-parse --short HEAD') do (
     set VAR=%%a
-    )
+)
 
 mkdir output
 move /Y windows_app_lifecycle.tgz output\windows_app_lifecycle-%VAR%.tgz || exit /b 1
